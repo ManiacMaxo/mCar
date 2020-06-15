9ad07f7d11bbb79a3ab49aa4f8f0c5b87564d3ca
Victor <victor@gorchilov.com>
2020-06-15 20:18:00 +0300

    fix: replace all mistake

--- test.py
+++ test.py
@@ -10,31 +10,31 @@ class Car:
         self.left = DigitalOutputDevice(7)
         self.right = DigitalOutputDevice(8)
 
-    def forward(self, time=1, speed=1):
+    def forward(self, time=1):
         self.back.off()
         # assure that both pins aren't on at the same time
-        self.forw.value()
+        self.forw.on()
         sleep(time)
         self.forw.off()
 
-    def backward(self, time=1, speed=1):
+    def backward(self, time=1):
         self.forw.off()
         # assure that both pins aren't on at the same time
-        self.back.value()
+        self.back.on()
         sleep(time)
         self.back.off()
 
-    def left(self, time=1, speed=1):
+    def left(self, time=1):
         self.right.off()
         # assure that both pins aren't on at the same time
-        self.left.value()
+        self.left.on()
         sleep(time)
         self.left.off()
 
-    def right(self, time=1, speed=1):
+    def right(self, time=1):
         self.left.off()
         # assure that both pins aren't on at the same time
-        self.right.value()
+        self.right.on()
         sleep(time)
         self.right.off()
 
