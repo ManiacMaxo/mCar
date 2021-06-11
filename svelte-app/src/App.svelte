eb5eeaafd1dff9831433d4d1a99fc8f1b8f62193
Victor Gorchilov <victor@gorchilov.com>
2021-06-11 08:18:16 +0100

    [up] add svelte frontend

---
+++ svelte-app/src/App.svelte
@@ -0,0 +1,29 @@
+<script>
+    import io from 'socket.io-client'
+    import Joystick from './Joytstick.svelte'
+    const socket = io()
+
+    socket.on('connect', () => {
+        socket.emit('connection', { data: "I'm connected!" })
+    })
+</script>
+
+<main>
+    <img class="video-feed" src="/video-feed" alt="video feed" />
+    <Joystick {socket} />
+</main>
+
+<style>
+    main {
+        display: flex;
+        flex-direction: column;
+        height: 100%;
+        justify-content: center;
+        align-items: center;
+    }
+
+    .video-feed {
+        height: 400px;
+        aspect-ratio: 16 / 10;
+    }
+</style>
---
+++ svelte-app/src/Joytstick.svelte
@@ -0,0 +1,47 @@
+<script>
+    import nipplejs from 'nipplejs'
+
+    export let socket = null
+
+    Number.prototype.map = function (in_min, in_max, out_min, out_max) {
+        return (
+            ((this - in_min) * (out_max - out_min)) / (in_max - in_min) +
+            out_min
+        )
+    }
+
+    const size = Math.min(window.innerWidth * 0.8, 200)
+    const radius = parseInt(size / 2)
+
+    const joystick = nipplejs.create({
+        create: document.querySelector('#joystick'),
+        color: 'blue',
+        mode: 'static',
+        // position: { left: '50%', top: '50%' },
+        dynamicPage: true,
+        size,
+    })
+
+    joystick.on('move', () => {
+        const { x, y } = joystick[0].frontPosition
+        socket.emit(
+            'control',
+            -x.map(-radius, radius, -1, 1),
+            -y.map(-radius, radius, -1, 1)
+        )
+    })
+
+    joystick.on('end', () => {
+        socket.emit('stop')
+    })
+</script>
+
+<div id="joystick" />
+
+<style>
+    #joystick {
+        position: relative;
+        height: 100%;
+        width: 100%;
+    }
+</style>
