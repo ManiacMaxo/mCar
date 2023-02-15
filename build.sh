acfd4a34f95ca0b73928ddaf86125b86e4524ce5
Victor Gorchilov <victor@gorchilov.com>
2023-02-15 21:28:35 +0200

    [up] dockerfiles for arm & x86 cross compilation host

--- build.sh
+++ build.sh
@@ -1,5 +1,29 @@
-#!/bin/bash
+#!/bin/bash -e
 
-NUM_CPUS=${NUM_CPUS:-$(sysctl -n hw.ncpu)}
-export CROSS_BUILD_OPTS="--build-arg NUM_CPUS=${NUM_CPUS}"
-cross build
+if [[ $1 == -* || !$1 ]]; then
+    ARCHITECTURE=$(uname -m)
+else
+    ARCHITECTURE=$1
+fi
+
+export DOCKER_SCAN_SUGGEST=false
+export CROSS_CONFIG=$(mktemp)
+
+echo $CROSS_CONFIG
+echo '[target.armv7-unknown-linux-gnueabihf]' >$CROSS_CONFIG
+
+case "$ARCHITECTURE" in
+arm* | aarch64)
+    echo 'image = "cross-compiler-mcar-arm"' >>$CROSS_CONFIG
+    docker build -t cross-compiler-mcar-arm -f Dockerfile.arm .
+    docker run --rm -v $(pwd):/src cross-compiler-mcar-arm /bin/bash -c "cd /src && cargo build ${@} --target armv7-unknown-linux-gnueabihf"
+    ;;
+*)
+    echo 'image = "cross-compiler-mcar-x86_64"' >>$CROSS_CONFIG
+    docker pull ghcr.io/cross-rs/armv7-unknown-linux-gnueabihf:latest --platform linux/amd64 >/dev/null
+    docker build -t cross-compiler-mcar-x86_64 -f Dockerfile.x86_64 .
+    cross build ${@} --target armv7-unknown-linux-gnueabihf
+    ;;
+esac
+
+rm $CROSS_CONFIG
